ba9ea93b49e0ae6f341fd949fba772ab5872158d
zakiabdess <zakiabdessemed35@gmail.com>
2024-09-19 22:22:56 +0100

    Done Hero section

--- components/BackgroundSlider.tsx
+++ components/BackgroundSlider.tsx
@@ -8,42 +8,99 @@ import bgPhoto2 from "../public/bg-2.png";
 import Image from "next/image";
 
 import "swiper/css";
+import { Autoplay } from "swiper/modules";
+import { CircleHelp, Globe, Menu } from "lucide-react";
 
 export default function BackgroundSlider() {
   return (
-    <div className="relative w-full h-screen">
+    <div className="flex flex-col justify-center items-center w-full h-screen">
+      <nav className="absolute top-0 z-50 flex items-center sm:justify-center justify-between w-full my-4">
+        <div className="flex justify-between items- md:w-2/3 w-5/6 space-x-6 ">
+          <a href="/">
+            <Image
+              src="/logo-opera.svg"
+              alt="Logo"
+              width={150}
+              height={150}
+              className="object-contain"
+            />
+          </a>
+
+          <div className="flex justify-center items-center space-x-4 xl:hidden max-md:w-1/4">
+            <button className="bg-transparent xl:block hidden">
+              <CircleHelp color="white" />
+            </button>
+
+            <button className="bg-transparent">
+              <Globe color="white" />
+            </button>
+
+            <button>
+              <Menu color="white" height={30} width={30} />
+            </button>
+          </div>
+
+          <div className="xl:flex justify-around items-center w-2/3 space-x-12 hidden">
+            <ul className="flex justify-evenly items-center space-x-12">
+              <li className="font-sans">
+                <a href="/">Acceuil</a>
+              </li>
+              <li className="font-sans">
+                <a href="#Qui" className="whitespace-nowrap">
+                  Qui sommes nous
+                </a>
+              </li>
+              <li className="font-sans">
+                <a href="#Qui">Produits</a>
+              </li>
+              <li className="font-sans">
+                <a href="#Blogs">Blog&Vidéos</a>
+              </li>
+              <li className="font-sans">
+                <a href="#Contact">Contact</a>
+              </li>
+            </ul>
+            <div className="flex justify-center items-center space-x-4">
+              <button className="bg-transparent">
+                <CircleHelp color="white" />
+              </button>
+
+              <button className="bg-transparent">
+                <Globe color="white" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </nav>
       {/* Swiper Setup */}
       <Swiper
         centeredSlides={true}
         autoplay={{
-          delay: 1000,
+          delay: 3000,
           disableOnInteraction: false,
         }}
-        className="w-full h-full z-0 mySwiper">
+        modules={[Autoplay]}
+        className="w-full h-full z-0 myswiper">
         <SwiperSlide>
-          <AspectRatio ratio={16 / 9}>
-            <Image
-              src={bgPhoto}
-              alt="Photo by Drew Beamer"
-              fill
-              className="object-cover w-full h-full"
-            />
-          </AspectRatio>
+          <Image
+            src={bgPhoto}
+            alt="Photo by Drew Beamer"
+            fill
+            className="object-cover max-w-full h-auto"
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <AspectRatio ratio={16 / 9}>
-            <Image
-              src={bgPhoto2}
-              alt="Photo by Drew Beamer"
-              fill
-              className="object-cover w-full h-full"
-            />
-          </AspectRatio>
+          <Image
+            src={bgPhoto2}
+            alt="Photo by Drew Beamer"
+            fill
+            className="object-cover max-w-full w-full h-auto"
+          />
         </SwiperSlide>
       </Swiper>
 
       {/* Overlay content */}
-      <div className="absolute inset-0 z-20 flex items-center justify-center">
+      <div className="absolute inset-0 z-20 flex items-center justify-around xl:w-2/3 m-4">
         <HeroSection />
       </div>
     </div>
--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -6,16 +6,16 @@ import Logo from "../public/logo-opera.svg";
 function HeroSection() {
   return (
     <div className="flex flex-col justify-start items-start">
-      <h1 className="text-5xl font-semibold flex flex-col justify-start items-center font-sans">
+      <h1 className="md:text-5xl text-4xl font-bold flex flex-col justify-start items-center font-sans">
         Exprimez votre style avec{" "}
       </h1>
-      <h2 className="flex justify-center items-center text-5xl font-semibold font-sans relative bottom-6">
+      <h2 className="flex justify-center items-center md:text-5xl text-4xl font-bold font-sans relative bottom-6">
         <Image
           src={Logo}
           height={191}
           width={150}
           alt="Logo"
-          className="relative right-1"
+          className="relative right-1 height-[120px] width-[100px] md:height-[150px] md:width-[120px]"
         />
         Peinture
       </h2>
@@ -24,10 +24,10 @@ function HeroSection() {
         Une touche d'élégance et de durabilité pour chaque espace
       </p>
 
-      <div className="space-x-2">
+      <div className="space-x-2 max-sm:space-y-2 xs:flex justify-center items-center">
         <Button
           variant="default"
-          className="bg-primary-ui hover:bg-primary-ui text-[#171717] font-medium px-4 py-2 rounded-full">
+          className="hover:bg-gray-200 bg-primary-ui text-[#171717] font-medium px-4 py-2 rounded-full">
           Visitez nos travaux
         </Button>
         <Button
