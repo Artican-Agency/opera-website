715a729a5e88ee27a8dc092ef48cbb39e5ce851d
zakiabdess <zakiabdessemed35@gmail.com>
2024-09-19 19:12:57 +0100

    Fixed need somechanges bug of no auto play

--- components/BackgroundSlider.tsx
+++ components/BackgroundSlider.tsx
@@ -3,7 +3,11 @@ import { AspectRatio } from "@/components/ui/aspect-ratio";
 import { SwiperSlide, Swiper } from "swiper/react";
 import HeroSection from "./HeroSection";
 import bgPhoto from "../public/bg-1.png";
+import bgPhoto2 from "../public/bg-2.png";
+
 import Image from "next/image";
+
+import "swiper/css";
 
 export default function BackgroundSlider() {
   return (
@@ -12,10 +16,10 @@ export default function BackgroundSlider() {
       <Swiper
         centeredSlides={true}
         autoplay={{
-          delay: 5000, // 5 seconds interval
+          delay: 1000,
           disableOnInteraction: false,
         }}
-        className="w-full h-full">
+        className="w-full h-full z-0 mySwiper">
         <SwiperSlide>
           <AspectRatio ratio={16 / 9}>
             <Image
@@ -29,19 +33,17 @@ export default function BackgroundSlider() {
         <SwiperSlide>
           <AspectRatio ratio={16 / 9}>
             <Image
-              src={bgPhoto}
+              src={bgPhoto2}
               alt="Photo by Drew Beamer"
               fill
               className="object-cover w-full h-full"
             />
           </AspectRatio>
         </SwiperSlide>
-
-        {/* Add more slides as needed */}
       </Swiper>
 
       {/* Overlay content */}
-      <div className="absolute inset-0 flex items-center justify-center">
+      <div className="absolute inset-0 z-20 flex items-center justify-center">
         <HeroSection />
       </div>
     </div>
