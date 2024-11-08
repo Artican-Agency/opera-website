46c2c64903e048b069881236e2f0902abdb4b6cd
zak <zakiabdessemed35@gmail.com>
2024-11-08 20:37:26 +0100

    FIXED ROUNDED

--- components/Scroller.tsx
+++ components/Scroller.tsx
@@ -43,7 +43,7 @@ const Scroller = () => {
   return (
     <div className="relative racesWrapper1">
       <div className="flex flex-col items-center justify-center space-y-4 races1 xl:mx-56">
-        <div className="w-full max-w-[756px] h-56 overflow-hidden rounded-[2.5rem]">
+        <div className="w-full max-w-[756px] h-56 overflow-hidden lg:rounded-[2.5rem]">
           <Image
             className="w-full h-full object-cover bg-center"
             ref={containerRef}
