ab007fcab33938c349216c6bee6aacc4a5729ab0
zak <zakiabdessemed35@gmail.com>
2024-11-20 23:16:22 +0100

    Added real stats

--- components/StatSection.tsx
+++ components/StatSection.tsx
@@ -5,21 +5,21 @@ function StatSection() {
     <div className="bg-primary-ui md:max-w-[700px] max-lg:hidden rounded-full flex justify-evenly items-center relative bottom-12 xl:left-1/3 md:left-1/4 z-20">
       <div className="p-4">
         <h2 className="font-sans text-sm text-black font-semibold">
-          Visiteurs
+          Point ventes
         </h2>
-        <h1 className="font-sans text-4xl font-bold text-black">179 K</h1>
+        <h1 className="font-sans text-4xl font-bold text-black">80</h1>
       </div>
       <div className="p-4">
         <h2 className="font-sans text-sm text-black font-semibold">
-          Stat Label
+          Produits fabriqués
         </h2>
-        <h1 className="font-sans text-4xl font-bold text-black">9.2 %</h1>
+        <h1 className="font-sans text-4xl font-bold text-black">60+</h1>
       </div>
       <div className="p-4">
         <h2 className="font-sans text-sm text-black font-semibold">
-          Stat Label
+          Expérience
         </h2>
-        <h1 className="font-sans text-4xl font-bold text-black">36 K</h1>
+        <h1 className="font-sans text-4xl font-bold text-black">10 ans</h1>
       </div>
     </div>
   );
